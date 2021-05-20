5268b6f69471a02ab0406b2c0ba63c3efe9721bc
SudoSmart <simplebug4@gmail.com>
2021-05-20 17:22:13 +0530

    random change 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Random change
+          Random change 2
         </a>
       </header>
     </div>
